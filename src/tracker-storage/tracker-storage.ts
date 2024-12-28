94567f0d7ecea433e6cf41a99ea42b077bff4d74
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-12-28 14:52:00 +0200

    feat - get chunks

--- src/tracker-storage/tracker-storage.ts
+++ src/tracker-storage/tracker-storage.ts
@@ -27,4 +27,44 @@ export class TrackerStorageInitializer {
             return {ok: false, data: err};
         }
     }
+    getChunks = async(): Promise<TrackerStorageResponse> => {
+        try{
+            const res = await fetch("https://tracker-storage.vercel.app/api/chunks", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.API_KEY}`
+                },
+            });
+
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const jsonData = await res.json();
+
+            return {ok: true, data:jsonData}
+        }catch(err){
+            return {ok: false, data: err};
+        }
+    }
+    getChunk = async(id: string): Promise<TrackerStorageResponse> => {
+        try{
+            const res = await fetch(`https://tracker-storage.vercel.app/api/chunks/${id.toString()}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.API_KEY}`
+                },
+            });
+
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const jsonData = await res.json();
+            
+            return {ok: true, data:jsonData}
+        }catch(err){
+            return {ok: false, data: err};
+        }
+    }
 }
